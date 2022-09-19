3aa1c06bfd90e079ef995790a4823fde6a7c88d2
Archy de Berker <archy@carbonchain.io>
2022-09-19 21:15:27 +0100

    Run black

--- example_pages.py
+++ example_pages.py
@@ -7,19 +7,24 @@ class IntroPage(Page):
     def render(self) -> dict:
         st.title("Intro")
         st.write("This is the intro page")
-        accept = st.radio("Do you understand that", options=['Yes', "No", "Can you repeat the question"])
+        accept = st.radio(
+            "Do you understand that",
+            options=["Yes", "No", "Can you repeat the question"],
+        )
         return dict(accept=accept)
 
 
 class FirstPageOfQuestions(Page):
     def render(self) -> dict:
-        st.title('Question Page')
+        st.title("Question Page")
         st.write("We will have some questions here")
 
         # We can access the other page state via session storage if we know what it is called
         # This is presumably not a very robust solution
 
-        did_they_accept = st.session_state["page_state"]['intro']['accept']
+        did_they_accept = st.session_state["page_state"]["intro"]["accept"]
         st.write(f"On the previous page you selected {did_they_accept}")
-        fave_bean = st.selectbox("What is your favourite jelly bean colour", ['Red', 'Orange', 'Green'])
+        fave_bean = st.selectbox(
+            "What is your favourite jelly bean colour", ["Red", "Orange", "Green"]
+        )
         return dict(fave_bean=fave_bean)
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import components
 import streamlit as st
 
+import components
 import example_pages
 
-st.markdown("<style>" + open("static/style.css").read() + "</style>", unsafe_allow_html=True)
+st.markdown(
+    "<style>" + open("static/style.css").read() + "</style>", unsafe_allow_html=True
+)
 
-
-intro = example_pages.IntroPage(name='intro')
-data = example_pages.FirstPageOfQuestions(name='questions')
+intro = example_pages.IntroPage(name="intro")
+data = example_pages.FirstPageOfQuestions(name="questions")
 
 wizard = components.Wizard(pages=[intro, data])
 
